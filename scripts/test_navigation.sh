450340644e5bedb466f96864fa4c73c5b60c437c
robond <g@georgiechallis.com>
2020-05-14 15:24:19 -0700

    fix: Add launch files to test_navigation shell script

--- scripts/test_navigation.sh
+++ scripts/test_navigation.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-
 #add turtlebot_world.launch
+xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch" &
 #add amcl_demo.launch
+xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch" &
 #add view_navigation.launch
+xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch"
